ad58be7e3c50898dc033762fc5939a814ee36777
wfalcone <wfalcone@osocna.com.ar>
2022-05-12 00:37:17 -0300

    arreglo section ejercicio js01 unidad 02

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 for (let i = 11; i >= 9 ; i--) {
-    document.write(`<div><br><b>La tabla del ${i}:</b></br></div>`);
+    document.write(`<section><br><b>La tabla del ${i}:</b></br></section>`);
     for (let j = 1; j < 10; j++) {
-        document.write(`<div class="tablas">${i} x ${j} : ${i*j}</div>`);
+        document.write(`<section class="tablas">${i} x ${j} : ${i*j}</section>`);
     }
 }
